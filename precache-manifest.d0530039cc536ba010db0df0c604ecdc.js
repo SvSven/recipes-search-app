9a331cd841e00ba51de4c8c5d3a44e92b7f94d0a
Sven van Steenis <sven.van.steenis@no.experis.com>
2019-09-30 21:19:28 +0200

    Updates

---
+++ precache-manifest.d0530039cc536ba010db0df0c604ecdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdde1c08e5cc719b870cde35c1d8164e",
+    "url": "/recipes-search-app/index.html"
+  },
+  {
+    "revision": "505e815223b5887656e7",
+    "url": "/recipes-search-app/static/css/2.cf291e2b.chunk.css"
+  },
+  {
+    "revision": "19b588d487a442ca179a",
+    "url": "/recipes-search-app/static/css/main.86c50c81.chunk.css"
+  },
+  {
+    "revision": "505e815223b5887656e7",
+    "url": "/recipes-search-app/static/js/2.88e702c6.chunk.js"
+  },
+  {
+    "revision": "19b588d487a442ca179a",
+    "url": "/recipes-search-app/static/js/main.c8efbf93.chunk.js"
+  },
+  {
+    "revision": "2b4f480bfe9a877af365",
+    "url": "/recipes-search-app/static/js/runtime-main.54b98b76.js"
+  }
+]);
